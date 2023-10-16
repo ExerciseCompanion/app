38ecfcecb20913841571a50128620684d23d0593
chris <clee48@hawaii.edu>
2023-10-16 08:36:49 -1000

    REORGANIZED

--- run-chrome.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-su -c "export DISPLAY=:0 && cd /home/chris/projects-synced/school/MAD/final/app/exercise_companion && flutter run -d web-server --web-hostname 0.0.0.0 --web-port 8989" chris 
-#
-#cd $HOME/projects-synced/school/MAD/final/app/exercise_companion && flutter run
